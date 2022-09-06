715afbaf4566b610f45ff4bc96cebe718d9dc0e9
Kunduz <KunduzNbkva@gmail.com>
2022-09-06 22:55:59 +0600

    add rounded image view, finish design of main player screen

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.musicplayer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
